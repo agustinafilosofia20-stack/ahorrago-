f5746cda6d6e82cb48983ef2d0fa7fe87ccb0b8a
Maria Agustina <agustina.filosofia@gmail.com>
2026-02-24 21:58:41 -0300

    Conectar manifest correctamente

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,27 +1,13 @@
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 export const metadata = {
   title: "Ahorrago",
-  description: "Ofertas, descuentos y productos en toda Argentina",
+  description: "App para ahorrar y gestionar finanzas",
+  manifest: "/manifest.json",
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang="es">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
